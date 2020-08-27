ec1173653ac820974d3eb83e304d3d992ee63726
mejs <vladimirvince@gmail.com>
2020-08-27 15:21:25 +0200

    update updategeojson.sh with EN

--- test/updategeojson.sh
+++ test/updategeojson.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 bash google2geojson.sh
+bash google2geojsonEN.sh
 git add korona-test.json
-git commit -m "korona-test.json updated"
+git add korona-testEN.json
+git commit -m "korona-test and korona-test.json updated"
 git push origin master
