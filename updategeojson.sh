d1487daf2384ce8933831500b608752f6e27f61d
mejs <vladimirvince@gmail.com>
2020-09-10 18:03:17 +0200

    fixed index.html

--- updategeojson.sh
+++ updategeojson.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 bash google2geojsonHR.sh
 bash google2geojsonEN.sh
+bash google2geojsonDE.sh
 git add korona.json
 git add koronaEN.json
-git commit -m "korona.json and koronaEN.json updated"
+git add koronaDE.json
+git commit -m "korona.json, koronaEN.json and koronaDE.json updated"
 git push origin master
