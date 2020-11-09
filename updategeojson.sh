4f6f053bd21f6cce7b75376d21df3a3d4bd14c2a
mejs <vladimirvince@gmail.com>
2020-11-08 20:36:53 -0500

    added Lee

--- updategeojson.sh
+++ updategeojson.sh
@@ -2,8 +2,10 @@
 bash google2geojsonHR.sh
 bash google2geojsonEN.sh
 bash google2geojsonDE.sh
+bash google2geojsonIT.sh
 git add korona.json
 git add koronaEN.json
 git add koronaDE.json
-git commit -m "korona.json, koronaEN.json and koronaDE.json updated"
+git add koronaIT.json
+git commit -m "korona.json, koronaEN.json, koronaDE.json and koronaIT.json updated"
 git push origin master
