a080b1d314e79e51deff2510e7a6ae32bc463a7c
mejs <vladimirvince@gmail.com>
2020-11-21 11:25:06 -0500

    antigen

--- updategeojson.sh
+++ updategeojson.sh
@@ -3,9 +3,11 @@ bash google2geojsonHR.sh
 bash google2geojsonEN.sh
 bash google2geojsonDE.sh
 bash google2geojsonIT.sh
+bash google2geojson_antigen.sh
 git add korona.json
 git add koronaEN.json
 git add koronaDE.json
 git add koronaIT.json
-git commit -m "korona.json, koronaEN.json, koronaDE.json and koronaIT.json updated"
+git add korona_antigen.json
+git commit -m "korona.json, korona_antigen.json, koronaEN.json, koronaDE.json and koronaIT.json updated"
 git push origin master
